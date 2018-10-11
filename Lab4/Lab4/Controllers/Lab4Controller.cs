f383dd7e39965a2e266c6b54e82fcd2f6475eeef
CptnKoser <36827338+CptnKoser@users.noreply.github.com>
2018-10-10 23:48:12 -0400

    Lab 4 pushed

---
+++ Lab4/Lab4/Controllers/Lab4Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WaffleGenerator;
+using System.Web;
+using Microsoft.AspNetCore.Html;
+
+namespace Lab4.Controllers
+{
+    public class Lab4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Waffle()
+        {
+            var waffle = WaffleEngine.Html(2, true, false);
+            return View(new HtmlString(waffle));
+        }
+    }
+}
